756a9c977cab849b3363a3f89030deae71efae67
nereVar34 <hunter34r@mail.ru>
2023-01-10 17:25:33 +0300

    Add comma.

--- main.cs
+++ main.cs
@@ -1,14 +1,14 @@
 using System;
 
-namespace Git
-{
-    class Git
-    {
-        static void Main(string[] args)
-        {
+ namespace Git
+ {
+     class Git
+     {
+         static void Main(string[] args)
+         {
             System.Console.WriteLine("Enter your name: ");
             string name = Console.ReadLine();
-            System.Console.WriteLine($"Hello {name}!");
-        }
-    }
-}
+            System.Console.WriteLine($"Hello, {name}!");
+         }
+     }
+ }
