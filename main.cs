7f690d582ba53e79ea3bde0d98999643dd7a892f
nereVar34 <hunter34r@mail.ru>
2023-01-10 17:15:43 +0300

    Added a request for the user's last name.
    Signed-off-by: Alex <alex@example.com>

--- main.cs
+++ main.cs
@@ -6,9 +6,11 @@ namespace Git
     {
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Enter your name: ");
-            string name = Console.ReadLine();
-            System.Console.WriteLine($"Hello {name}!");
+            System.Console.WriteLine("Enter your first name: ");
+            string first_name = Console.ReadLine();
+            System.Console.WriteLine("Now enter your last name: ");
+            string last_name = Console.ReadLine();
+            System.Console.WriteLine($"Hello {first_name} {last_name}!");
         }
     }
 }
